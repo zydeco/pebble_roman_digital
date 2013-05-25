d379e1c058972f1ef3214821c4f475718f02f763
Jesús A. Álvarez <zydeco@namedfork.net>
2013-05-25 12:41:10 +0800

    config options to not display seconds and change date format (M-D or D-M)

---
+++ src/config.h
@@ -0,0 +1,5 @@
+// comment line below to hide seconds display
+#define CONFIG_SHOW_SECONDS
+
+// use month-day date format, comment to use day-month
+#define CONFIG_DATE_MD
--- src/roman_digital.c
+++ src/roman_digital.c
@@ -1,13 +1,13 @@
 #include "pebble_os.h"
 #include "pebble_app.h"
 #include "pebble_fonts.h"
-
+#include "config.h"
 
 #define MY_UUID { 0xAC, 0xB9, 0x74, 0x69, 0xE8, 0x88, 0x49, 0x15, 0x9E, 0xC6, 0x2B, 0xC6, 0x0F, 0x58, 0x6F, 0x5C }
 PBL_APP_INFO(
   MY_UUID,
   "Roman Digital", "namedfork.net",
-  1, 0, /* App version */
+  1, 1, /* App version */
   RESOURCE_ID_IMAGE_MENU_ICON,
   APP_INFO_WATCH_FACE);
 
@@ -19,6 +19,22 @@ static struct {
   PblTm time;
 } g;
 
+#if defined(CONFIG_SHOW_SECONDS)
+// date, hour, minute, seconds
+#define SEP_HEIGHT 5
+#define TICK_UNIT SECOND_UNIT
+#define DATE_RECT GRect(4, 4, 136, 34)
+#define HOUR_RECT GRect(4, 33, 136, 83)
+#define MINUTE_RECT GRect(4, 111, 136, 29)
+#define SECOND_RECT GRect(4, 135, 136, 29)
+#else
+// date, hour, minute
+#define SEP_HEIGHT 7
+#define TICK_UNIT MINUTE_UNIT
+#define DATE_RECT GRect(4, 8, 136, 40)
+#define HOUR_RECT GRect(4, 41, 136, 91)
+#define MINUTE_RECT GRect(4, 125, 136, 35)
+#endif
 
 void draw_l(GContext *ctx, GRect rect) {
   int16_t w = rect.size.w / 4.f;
@@ -152,43 +168,48 @@ void bg_update_proc(Layer *me, GContext *ctx) {
 
 void date_update_proc(Layer *me, GContext *ctx) {
   graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, GRect(0, 0, me->bounds.size.w, 5), 0, GCornerNone);
-  graphics_fill_rect(ctx, GRect(0, me->bounds.size.h - 5, me->bounds.size.w, me->bounds.size.h), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(0, 0, me->bounds.size.w, SEP_HEIGHT), 0, GCornerNone);
   
-  GRect rect = GRect(4, 4, (me->bounds.size.w - 8)/2, me->bounds.size.h - 9);
-  draw_roman_number(g.time.tm_mon + 1, ctx, rect, -1);
+#if defined(CONFIG_DATE_MD)
+  int8_t date[2] = {g.time.tm_mon + 1, g.time.tm_mday};
+#else
+  int8_t date[2] = {g.time.tm_mday, g.time.tm_mon + 1};
+#endif
+
+  GRect rect = GRect(4, SEP_HEIGHT-1, (me->bounds.size.w - 8)/2, me->bounds.size.h - (SEP_HEIGHT*2) + 1);
+  draw_roman_number(date[0], ctx, rect, -1);
   rect.origin.x += (me->bounds.size.w - 8)/2;
-  draw_roman_number(g.time.tm_mday, ctx, rect, 1);
+  draw_roman_number(date[1], ctx, rect, 1);
 }
 
 void hour_update_proc(Layer *me, GContext *ctx) {
   graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, GRect(0, 0, me->bounds.size.w, 5), 0, GCornerNone);
-  graphics_fill_rect(ctx, GRect(0, me->bounds.size.h - 5, me->bounds.size.w, me->bounds.size.h), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(0, 0, me->bounds.size.w, SEP_HEIGHT), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(0, me->bounds.size.h - SEP_HEIGHT, me->bounds.size.w, me->bounds.size.h), 0, GCornerNone);
   
   int8_t hour = g.time.tm_hour;
   if (!clock_is_24h_style()) {
     hour %= 12;
   }
   if (hour == 0) hour = clock_is_24h_style() ? 24 : 12;
-  draw_roman_number(hour, ctx, GRect(8, 4, me->bounds.size.w - 16, me->bounds.size.h - 9), 0);
+  draw_roman_number(hour, ctx, GRect(8, SEP_HEIGHT-1, me->bounds.size.w - 16, me->bounds.size.h - (SEP_HEIGHT*2) + 1), 0);
 }
 
 void minute_update_proc(Layer *me, GContext *ctx) {
   graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, GRect(0, 0, me->bounds.size.w, 5), 0, GCornerNone);
-  graphics_fill_rect(ctx, GRect(0, me->bounds.size.h - 5, me->bounds.size.w, me->bounds.size.h), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(0, me->bounds.size.h - SEP_HEIGHT, me->bounds.size.w, me->bounds.size.h), 0, GCornerNone);
   
-  draw_roman_number(g.time.tm_min, ctx, GRect(0, 4, me->bounds.size.w, me->bounds.size.h - 9), 0);
+  draw_roman_number(g.time.tm_min, ctx, GRect(0, SEP_HEIGHT-1, me->bounds.size.w, me->bounds.size.h - (SEP_HEIGHT*2) + 1), 0);
 }
 
+#if defined(CONFIG_SHOW_SECONDS)
 void second_update_proc(Layer *me, GContext *ctx) {
   graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, GRect(0, 0, me->bounds.size.w, 5), 0, GCornerNone);
-  graphics_fill_rect(ctx, GRect(0, me->bounds.size.h - 5, me->bounds.size.w, me->bounds.size.h), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(0, me->bounds.size.h - SEP_HEIGHT, me->bounds.size.w, me->bounds.size.h), 0, GCornerNone);
   
-  draw_roman_number(g.time.tm_sec, ctx, GRect(0, 4, me->bounds.size.w, me->bounds.size.h - 9), 0);
+  draw_roman_number(g.time.tm_sec, ctx, GRect(0, SEP_HEIGHT-1, me->bounds.size.w, me->bounds.size.h - (SEP_HEIGHT*2) + 1), 0);
 }
+#endif
 
 void handle_init(AppContextRef ctx) {
   (void)ctx;
@@ -200,38 +221,61 @@ void handle_init(AppContextRef ctx) {
   g.layers.bg.update_proc = &bg_update_proc;
   layer_add_child(&g.window.layer, &g.layers.bg);
   
-  layer_init(&g.layers.date, GRect(4, 4, 136, 34));
+  layer_init(&g.layers.date, DATE_RECT);
   g.layers.date.update_proc = &date_update_proc;
   layer_add_child(&g.layers.bg, &g.layers.date);
   
-  layer_init(&g.layers.hour, GRect(4, 33, 136, 83));
+  layer_init(&g.layers.hour, HOUR_RECT);
   g.layers.hour.update_proc = &hour_update_proc;
   layer_add_child(&g.layers.bg, &g.layers.hour);
   
-  layer_init(&g.layers.minute, GRect(4, 111, 136, 29));
+  layer_init(&g.layers.minute, MINUTE_RECT);
   g.layers.minute.update_proc = &minute_update_proc;
   layer_add_child(&g.layers.bg, &g.layers.minute);
   
-  layer_init(&g.layers.second, GRect(4, 135, 136, 29));
+#if defined(CONFIG_SHOW_SECONDS)
+  layer_init(&g.layers.second, SECOND_RECT);
   g.layers.second.update_proc = &second_update_proc;
   layer_add_child(&g.layers.bg, &g.layers.second);
+#endif
   
   get_time(&g.time);
   window_stack_push(&g.window, true /* Animated */);
 }
 
-void handle_second_tick(AppContextRef ctx, PebbleTickEvent *t) {
+void handle_tick(AppContextRef ctx, PebbleTickEvent *t) {
   (void)t;
   get_time(&g.time);
-  layer_mark_dirty(&g.layers.bg);
+  bool update = 1;
+#if defined(CONFIG_SHOW_SECONDS)
+  layer_mark_dirty(&g.layers.second);
+  update = (g.time.tm_sec == 0);
+#endif
+  
+  // update minutes?
+  if (update) {
+    layer_mark_dirty(&g.layers.minute);
+    update = (g.time.tm_min == 0);
+  }
+  
+  // update hours?
+  if (update) {
+    layer_mark_dirty(&g.layers.hour);
+    update = (g.time.tm_hour == 0);
+  }
+  
+  // update date
+  if (update) {
+    layer_mark_dirty(&g.layers.date);
+  }
 }
 
 void pbl_main(void *params) {
   PebbleAppHandlers handlers = {
     .init_handler = &handle_init,
     .tick_info = {
-      .tick_handler = &handle_second_tick,
-      .tick_units = SECOND_UNIT
+      .tick_handler = &handle_tick,
+      .tick_units = TICK_UNIT
     }
   };
   app_event_loop(params, &handlers);
